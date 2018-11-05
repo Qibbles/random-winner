baa4dbece79557bc73e840ad6a27bdc482bfb55f
Qibbles <coffeecup@gmail.com>
2018-11-05 13:12:42 +0800

    Add files via upload

---
+++ Random Prize Winner.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import ttk
+
+#GUI
+win = tk.Tk()
+win.title("Random Winners")
+
+#labels
+winnerLabel = ttk.Label(win, text="No. Winners")
+winnerLabel.grid(column=0, row=0)
+
+csvLabel = ttk.Label(win, text="Upload File")
+csvLabel.grid(column=0, row=1)
+
+headerLabel = ttk.Label(win, text="Has Header?")
+headerLabel.grid(column=0, row=2)
+
+#textboxes
+winner = tk.StringVar()
+winnerBox = ttk.Entry(win, width=12, textvariable=winner)
+
+csv = tk.StringVar()
+csvBox = ttk.Entry(win, width=12, textvariable=csv)
+
+#checkbox
+
+#buttons
